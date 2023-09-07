c4f472f5ff7be5e8f38436807e834caf1c3ed3d5
nikobojs <n@nikobojs.com>
2023-09-07 19:21:32 +0200

    feat: upgrade mark overlay ui

--- renderers/markAreaP5Sketch.js
+++ renderers/markAreaP5Sketch.js
@@ -1,50 +1,69 @@
 // x, y, w, h
 let area = [0,0,0,0];
 let dragging = false;
-let alpha = 90;
+let alpha = 180;
 let closing = false;
 let resultRect = null;
 let fillColor = null;
 let strokeColor = null;
 
 function close() {
-  alpha -= 8;
+  alpha -= 16;
 }
 
 const sketch = (p) => {
 
-  strokeColor = p.color(80, 110, 180, alpha)
-  fillColor = p.color(10, 10, 100, alpha)
+  strokeColor = p.color(80, 110, 180, 1)
+  fillColor = p.color(23, 29, 38, alpha)
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
-    p.stroke(strokeColor);
-    p.strokeWeight(2);
-    p.fill(fillColor)
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(fillColor);
+    window.focus();
   }
 
 
   p.draw = () => {
     if (alpha < 0) {
       p.clear(255, 255, 255, 1);
-      setTimeout(() => {
-        window.electronAPI.areaMarked(resultRect);
-        window.close();
-      }, 500);
+      window.electronAPI.areaMarked(resultRect);
       closing = false;
+      window.close();
       p.noLoop();
     }
 
     if (dragging || closing) {
+      p.clear(255, 255, 255, 1);
       fillColor.setAlpha(alpha);
       strokeColor.setAlpha(alpha);
-      p.clear(255, 255, 255, 1);
-      p.fill(fillColor)
-      p.rect(area[0], area[1], area[2], area[3]);
-    }
+      p.noStroke();
+      p.fill(fillColor);
 
-    if (closing) {
-      alpha = alpha - 32;
+      const minX = Math.min(area[0], area[2] + area[0]);
+      const minY = Math.min(area[1], area[3] + area[1]);
+      const maxX = Math.max(area[0], area[2] + area[0]);
+      const maxY = Math.max(area[1], area[3] + area[1]);
+      const w = Math.abs(area[2]);
+      const h = Math.abs(area[3]);
+
+
+      p.rect(0, 0, p.windowWidth, minY)
+      p.rect(0, minY, minX, h)
+      p.rect(maxX, minY, p.windowWidth - maxX, h)
+      p.rect(0, maxY, p.windowWidth, p.windowHeight - maxY)
+
+      p.noFill();
+      p.stroke(strokeColor)
+      p.strokeWeight(2);
+
+      p.rect(minX, minY, w, h)
+
+      if (closing) {
+        alpha = alpha - 32;
+      }
+    } else {
+      p.clear(255, 255, 255, 1);
+      p.background(fillColor);
     }
   }
 
